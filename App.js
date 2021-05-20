e8938d511b603aadc12d46af74d0159e653bb634
Bartek Karelus <bartek.karelus@gmail.com>
2021-05-20 20:06:06 +0200

    add safe Area

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <Text>First app in react native Bolek1906</Text>
+          <Text>abcd</Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "black"
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    fontWeight: "bold",
+    backgroundColor: "blue",
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
