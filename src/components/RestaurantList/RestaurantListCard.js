f5c3da9aba5690f4fcee2a617c313eae9e8dd4ce
Bartek Karelus <bartek.karelus@gmail.com>
2021-05-23 12:13:10 +0200

    map rating and display stars

--- src/components/RestaurantList/RestaurantListCard.js
+++ src/components/RestaurantList/RestaurantListCard.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from "styled-components/native";
-import star from "../../../assets/star.svg"
-import { Card, Title } from "react-native-paper"
+import { Card } from "react-native-paper"
 import burger from "../../../assets/burgers.jpg"
-import { View } from 'react-native';
-import { SvgXml } from "react-native-svg"
+import { Text, View, Image } from 'react-native';
+import star from "../../../assets/star.png"
 
 
 const CardComponent = styled(Card)
@@ -14,41 +13,58 @@ border-radius: 5px;
 margin: 6px;
 background-color: #f8f9fa;
 `;
-const TitleComponent = styled(Title)
+const TitleComponent = styled(Text)
     `
-    font-family: ${(props) => props.theme.fonts.body};
-padding: 10px;
-margin: auto
-
+font-family: ${(props) => props.theme.fonts.body};
+padding-top: 10px;
+font-size: ${(props) => props.theme.fontSizes.title}
 `;
 
 const Info = styled(View)
     `
-padding: ${(props) => props.theme.space[3]}
+display:flex;
+flex-direction: column;
 `;
 
-const Adress = styled.p`
-text-align: left;
+const Adress = styled(Text)
+    `
 font-family: ${(props) => props.theme.fonts.heading};
 font-size:${(props) => props.theme.fontSizes.caption} ;
+margin-top: 10px;
 `
+
+const Star = styled(Image)
+    `
+margin-top:10px
+`;
+const Row = styled(View)
+    `
+display: flex;
+flex-direction: row;
+`;
 const RestaurantListCard = ({ Restaurant = {} }) => {
     const {
         name = "Some restaurant",
-        isOpen = true,
         icon,
         photo = burger,
         adress = "random 1100",
         rating = 4,
     } = Restaurant
 
+    const ratingStar = Array.from(new Array(Math.ceil(rating)))
+    const mapRating = ratingStar.map(() => (
+        <Star style={{ width: 20, height: 20, margin: 1 }} source={star} />
+    ))
+
     return (
-        <CardComponent>
+        <CardComponent key={name}>
             <Card.Cover source={photo} />
             <Card.Content>
                 <Info>
                     <TitleComponent>{name}</TitleComponent>
-                    <SvgXml xml={star} />
+                    <Row>
+                        {mapRating}
+                    </Row>
                     <Adress>{adress}</Adress>
                 </Info>
             </Card.Content>
