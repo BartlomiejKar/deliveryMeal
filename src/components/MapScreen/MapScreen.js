15b55e446fa5b3c8241e70dba938611c0848d70a
Bartek Karelus <bartek.karelus@gmail.com>
2021-05-26 18:02:37 +0200

    add map screen

--- src/components/MapScreen/MapScreen.js
+++ src/components/MapScreen/MapScreen.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SafeAreaComponent from "../SafeArea/SafeArea";
-import MapView from 'react-native-maps';
-import { View } from "react-native"
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { View, Text } from "react-native"
+import { RestaurantContext } from '../../services/Restaurants/RestaurantContext';
 
 const MapScreen = () => {
+    const { restaurants } = useContext(RestaurantContext)
     return (
         <SafeAreaComponent>
-            <View >
-                <MapView />
+            <View  >
+                <MapView
+                    initialRegion={{
+                        latitude: 51.219448,
+                        longitude: 4.402464,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.02,
+                    }}
+                    style={{ height: "100%" }}>
+                    {restaurants.map(el => {
+                        return (
+                            <Marker
+                                key={el.name}
+                                title={el.name}
+                                coordinate={{
+                                    latitude: el.geometry.location.lat,
+                                    longitude: el.geometry.location.lng
+                                }}
+
+                            >
+                                <Callout>
+                                    <Text>{el.name} <Text style={{ color: "blue" }}>Rating:</Text> {el.rating}</Text>
+                                </Callout>
+                            </Marker>
+                        )
+                    })}
+                </MapView>
             </View>
         </SafeAreaComponent>
     )
